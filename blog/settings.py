fc7619951c4c5d791b4d915342de34cd6d4aba62
dehemart <martins.denis@gmail.com>
2021-05-08 22:27:22 +0000

    Ambiente

--- blog/settings.py
+++ blog/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g56fvuq(#@_4l=6-&2#^2exot=0^=b&mg+-affn@^q6vr4em6n'
+SECRET_KEY = '=j86@$a*53xm5kxuwi&5)v5tgs(o+wyb3+b=njhclr2js_k^!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
